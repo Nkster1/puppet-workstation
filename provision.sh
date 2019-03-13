4c34bf535c1f0d542cbc3391400c5845e6086a30
Nkster1 <36925042+Nkster1@users.noreply.github.com>
2019-03-13 15:30:48 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -8,4 +8,4 @@ sudo /opt/puppetlabs/bin/puppet module install bashtoni-timezone
 sudo /opt/puppetlabs/bin/puppet module install puppet-archive
 
 echo "Setting up workstation... "
-./update.sh
+sudo ./update.sh
