0ac5a564019acef0de921be74ba8451e9ff96950
Nkster1 <36925042+Nkster1@users.noreply.github.com>
2019-03-14 13:52:58 +0100

    Update puppet5_init.sh

--- puppet5_init.sh
+++ puppet5_init.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+ 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd ~ && wget https://apt.puppetlabs.com/puppet-release-xenial.deb
 sudo dpkg -i puppet-release-xenial.deb
 sudo apt-get update
 sudo apt-get install puppet-agent
+cd "$parent_path"
+sudo chmod +rwx ./provision.sh
+sudo chmod +rwx ./docker_setup.sh
+sudo chmod +rwx ./update.sh
+sudo chmod +rwx ./install_certificate.sh
