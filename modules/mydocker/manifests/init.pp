df2f3d577338685b94d540903765b02f44d859a9
niclas.mettenleiter <niclas.mettenleiter@steadforce.de>
2019-03-14 11:28:47 +0100

    bus

--- modules/mydocker/manifests/init.pp
+++ modules/mydocker/manifests/init.pp
@@ -6,19 +6,19 @@ class mydocker{
     #onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
   }
   exec { 'install-docker-1':
-    command => 'sudo apt-get install apt-transport-https ca-certificates curl software-properties-common',
+    command => "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
     require => exec['apt-get update']
   }
   exec { 'install-docker-2':
-    command => 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –',
+    command => "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –",
     require => exec['install-docker-1']
   }
   exec { 'install-docker-3':
-    command => 'sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable',
+    command => "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable",
     require => exec['install-docker-2']
   }
   exec { 'install-docker-4':
-    command => 'sudo apt-get install docker-ce',
-    require => exec['update', 'install-docker-3']
+    command => "sudo apt-get install docker-ce",
+    require => exec['install-docker-3']
   }
 }
