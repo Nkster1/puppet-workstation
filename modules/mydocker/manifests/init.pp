848fd767efe14a528597d45f189bf6a809659943
niclas.mettenleiter <niclas.mettenleiter@steadforce.de>
2019-03-14 11:57:21 +0100

    added refreshonly

--- modules/mydocker/manifests/init.pp
+++ modules/mydocker/manifests/init.pp
@@ -7,14 +7,17 @@ class mydocker{
   }
   exec { 'install-docker-2':
     command => "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –",
-    subscribe => Exec['apt-get update']
+    subscribe => Exec['apt-get update'],
+    refreshonly => true,
   }
   exec { 'install-docker-3':
     command => "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable",
-    subscribe => Exec['install-docker-2']
+    subscribe => Exec['install-docker-2'],
+    refreshonly => true,
   }
   exec { 'install-docker-4':
     command => "sudo apt-get install docker-ce",
-    subscribe => Exec['install-docker-3']
+    subscribe => Exec['install-docker-3'],
+    refreshonly => true, 
   }
 }
