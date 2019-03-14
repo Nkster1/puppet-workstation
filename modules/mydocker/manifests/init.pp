c1eb1f082ce2cd3b20f032c778a0ec9d8f1a1c4b
niclas.mettenleiter <niclas.mettenleiter@steadforce.de>
2019-03-14 11:32:53 +0100

    fixed capital error

--- modules/mydocker/manifests/init.pp
+++ modules/mydocker/manifests/init.pp
@@ -7,18 +7,18 @@ class mydocker{
   }
   exec { 'install-docker-1':
     command => "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
-    require => exec['apt-get update']
+    require => Exec['apt-get update']
   }
   exec { 'install-docker-2':
     command => "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –",
-    require => exec['install-docker-1']
+    require => Exec['install-docker-1']
   }
   exec { 'install-docker-3':
     command => "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable",
-    require => exec['install-docker-2']
+    require => Exec['install-docker-2']
   }
   exec { 'install-docker-4':
     command => "sudo apt-get install docker-ce",
-    require => exec['install-docker-3']
+    require => Exec['install-docker-3']
   }
 }
